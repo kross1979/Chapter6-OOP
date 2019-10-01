8b600690d39dc4962559e4b21225200468859a4c
0273898 <0273898@KELCE-LABS13-P.pittstate.edu>
2019-10-01 12:10:58 -0500

    end of class

--- src/main/java/Chapter6OOP.java
+++ src/main/java/Chapter6OOP.java
@@ -11,5 +11,10 @@
 public class Chapter6OOP {
     public static void main(String[] args) {
         
+        Employee myEmployee = new Employee("John", "Smith", 4000, 0.20F, 500);
+        
+        float netPayForMonth = myEmployee.calculateMonthlyPay(0, 0);
+        
+        System.out.println(netPayForMonth);
     }
 }
